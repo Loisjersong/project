699650f61d7d78f34f9d75c724d50d56c25c17d6
Lois Gonzales <loisjersong@gmail.com>
2024-01-09 15:08:22 +0800

    added project

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Animate on Scroll
+window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    })
+  });
